acfb96be61b3e33c2f96dcd2595c5ce21a4b633d
Ryan Hou <ryanhau@outlook.com>
2020-11-05 14:57:47 -0600

    Update Ising

--- src/problems/Ising.py
+++ src/problems/Ising.py
@@ -12,33 +12,49 @@ Methods:
     get_info():
 
 Variables:
+    problem_type
+
     num_spin: the number of spins in this Ising problem
     edges: store all the edges in the Ising problem
     best_sol: store the best known solution to the problem
     best_config: store the corresponding spin configuration of the best known solution
 '''
 
+import os
+
 from src.problems.Problem import Problem
 
 class Ising(Problem):
     def __init__(self):
-        self.problem_type = 'Ising' # We support only Ising problem right now
+        self.problem_type = 'Ising'
         self.clear()
 
     # Method: clear current problem
     def clear(self):
         self.edges = []
         self.num_spin = 0
-        self.best_sol = float('inf')
+        self.best_sol = None
+        self.best_config = None
+
+    def addEdge(self, x1, x2, weight):
+        self.edges.append([x1, x2, weight])
+
+    def chookSamplePath(self, problem_size, instance):
+        # file to be read
+        targetFolder = f'tile_planting_2D_L_{problem_size}_p1_0.2_p2_0.5_p3_0.1'
+        targetFile = f'{targetFolder}_inst_{instance}.txt'
+        problemPath = os.path.join('data', 'Ising_chook', targetFolder, targetFile)
+        energyPath = os.path.join('data', 'Ising_chook', targetFolder, 'gs_energies.txt')
+        if os.path.isfile(problemPath) and os.path.isfile(energyPath):
+            return problemPath, energyPath
+
+        print('The given chook data does not exist, please check again.')
+        return None, None
 
     # import chook Ising problem
     def import_IsingChook(self, problem_size=4, instance=1):
         # clear any previous problem
         self.clear()
-
-        # file to be read
-        targetFolder = f'tile_planting_2D_L_{problem_size}_p1_0.2_p2_0.5_p3_0.1'
-        targetFile = f'{targetFolder}_inst_{instance}.txt'
 
         # read from file
         with open(f'data/Ising_chook/{targetFolder}/{targetFile}', 'r') as f:
